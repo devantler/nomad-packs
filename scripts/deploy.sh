17f6e78b31bf1b84065bb5c53361a44e9041f078
Nikolai Emil Damm <nikolaiemildam@icloud.comm>
2022-02-03 23:10:36 +0000

    Added wireguard to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
 docker stack deploy -c <(docker-compose -f ../stacks/portainer.yml config) portainer
 docker stack deploy -c <(docker-compose -f ../stacks/traefik.yml config) traefik
+# Move to Portainer when supported
 docker stack deploy -c <(docker-compose -f ../stacks/keepalived.yml config) keepalived
+docker stack deploy -c <(docker-compose -f ../stacks/wireguard.yml config) wireguard
