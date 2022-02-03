8aa03b34af554c27d16280ed2292cb5a4ef2b3b7
Nikolai Emil Damm <nikolaiemildam@icloud.comm>
2022-02-03 20:46:22 +0000

    update deploy.sh and cloudflare-ddns.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 docker stack deploy -c <(docker-compose -f ../stacks/portainer.yml config) portainer
 docker stack deploy -c <(docker-compose -f ../stacks/traefik.yml config) traefik
+docker stack deploy -c <(docker-compose -f ../stacks/keepalived.yml config) keepalived
