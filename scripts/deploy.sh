9dd2d07f1c7e93ca47ca4a73d67e5a16f56b540c
Nikolai Emil Damm <nikolaiemildam@icloud.comm>
2022-02-03 19:50:48 +0000

    Moved files around

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,2 @@
-docker stack deploy -c <(docker-compose -f ../stacks/container-management/portainer.yml config) portainer
-docker stack deploy -c <(docker-compose -f ../stacks/reverse-proxy/traefik.yml config) traefik
-docker stack deploy -c <(docker-compose -f ../stacks/high-availability/keepalived.yml config) keepalived
-docker stack deploy -c <(docker-compose -f ../stacks/vpn/wireguard.yml config) wireguard
+docker stack deploy -c <(docker-compose -f ../stacks/portainer.yml config) portainer
+docker stack deploy -c <(docker-compose -f ../stacks/traefik.yml config) traefik
