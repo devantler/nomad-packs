aa3a1f8613abd3693cf625c7725e491bbe73f38c
devantler <devantler>
2022-04-05 00:52:58 +0200

    Updates

--- scripts/init-swarm.sh
+++ scripts/init-swarm.sh
@@ -2,4 +2,5 @@
 # Init swarm
 docker swarm init
 docker network create -d overlay agent_network
-docker network create -d overlay traefik
+docker network create -d overlay public
+docker network create -d overlay auth
