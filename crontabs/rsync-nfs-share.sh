3e0a7f81f52e8c988d29563c81a426cb49f1fc3c
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-04-18 02:18:02 +0200

    fix rsync script

--- crontabs/rsync-nfs-share.sh
+++ crontabs/rsync-nfs-share.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -az --delete /home/devantler/nfs-share devantler@192.168.1.203:/home/devantler/nfs-share
+rsync -az --delete /home/devantler/nfs-share devantler@192.168.1.203:/home/devantler
